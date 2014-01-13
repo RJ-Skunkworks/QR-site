b096ccb72ca8628a1ad86b5b0a9069d58352351a
Roy Keyes <roy.coding@gmail.com>
2014-01-12 23:13:25 -0700

    Added https or no prefix option. Removed some debugging code.

--- routes.py
+++ routes.py
@@ -20,6 +20,7 @@ def about():
 
 @app.route('/qrrequest', methods=['POST'])
 def qrrequest():
+    session['prefix'] = request.form['prefix']
     session['URL'] = request.form['URL']
     return redirect(url_for('qrcode'))
 
@@ -27,7 +28,7 @@ def qrrequest():
 def qrcode():
     try:
         # Cookie test
-        return render_template('qrcode.html', imgURL = qrURL(session['URL']), URL = session['URL'])
+        return render_template('qrcode.html', imgURL = qrURL(session['prefix'],session['URL']), URL = session['URL'], prefix = session['prefix'])
     except KeyError:
         return redirect(url_for('nocookies'))
 
@@ -35,16 +36,13 @@ def qrcode():
 def nocookies():
     return '<p>Looks like your cookies might not be enabled.</p><p>We hate cookies too, but you need them to use our site...</p><p>Enable your cookies for our domain and go <a href="/">back<a/></p>'
 
-def qrURL(URL):
+def qrURL(prefix,URL):
     '''Render QR code and return image URL.'''
 
-    # Needs to be generalized to https
-    URL = 'http://'+URL
+    # TODO: Needs to be generalized to https
+    URL = prefix+URL
 
     tempURL = './static/img/temp-'+re.sub('\W','_',URL)+'.png'
-
-    print 'URL:',URL
-    print 'type(URL):',type(URL)
 
     img = qrgen.url2qr(URL,imgtype = 'png')
     img.save(tempURL)
