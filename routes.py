1a2fd6619f9348730da117435ea522cd0e6fea44
Roy Keyes <roy.coding@gmail.com>
2014-01-15 16:42:00 -0700

    Created basic poster info submission form. Added test poster png for now.

--- routes.py
+++ routes.py
@@ -19,11 +19,23 @@ def home():
 def about():
     return render_template('about.html')
 
+# Get user input to generate a QR code
 @app.route('/qrrequest', methods=['POST'])
 def qrrequest():
-    session['prefix'] = request.form['prefix']
-    session['URL'] = request.form['URL']
+    session['qr.prefix'] = request.form['prefix']
+    session['qr.URL'] = request.form['URL']
     return redirect(url_for('qrcode'))
+
+# Get user input to generate a poster page and QR code
+@app.route('/pagegen', methods=['POST'])
+def pagegen():
+    session['poster.title'] = request.form['title']
+    session['poster.authors'] = request.form['authors']
+    session['poster.contact'] = request.form['contact']
+    session['poster.conf'] = request.form['conf']
+    session['poster.date'] = request.form['date']
+    session['poster.abstract'] = request.form['abstract']
+    return redirect(url_for('posterpage'))
 
 def cookiecheck(f):
     '''Check if cookies are on.'''
@@ -38,13 +50,21 @@ def cookiecheck(f):
 @app.route('/qrcode')
 @cookiecheck
 def qrcode():
-    return render_template('qrcode.html', imgURL = qrURL(session['prefix'],session['URL']), URL = session['URL'], prefix = session['prefix'])
+    return render_template('qrcode.html', imgURL = qrURL(session['qr.prefix'],session['qr.URL']), URL = session['qr.URL'], prefix = session['qr.prefix'])
+
+@app.route('/posterpage')
+@cookiecheck
+def posterpage():
+    # For dev at this point just use a single page / URL
+    prefix = 'http://'
+    URL = '127.0.0.1:5000' + url_for('posterpage')
+    return render_template('posterpage.html', data = session, imgURL = qrURL(prefix,URL), URL=URL, prefix=prefix)
 
 @app.route('/nocookies')
 def nocookies():
     return '<p>Looks like your cookies might not be enabled.</p><p>We hate cookies too, but you need them to use our site...</p><p>Enable your cookies for our domain and go <a href="/">back<a/></p>'
 
-def qrURL(prefix,URL):
+def qrURL(URL,prefix='http'):
     '''Render QR code and return image URL.'''
     URL = prefix+URL
 
