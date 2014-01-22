cf0b2c2dee6d24924a45b348b7192a87ca65a733
Roy Keyes <roy.coding@gmail.com>
2014-01-22 11:05:30 -0700

    Minor changes to fix poster pdf url. Update to gitignore.

--- routes.py
+++ routes.py
@@ -68,7 +68,7 @@ def posterpage():
     # For dev at this point just use a single page / URL
     prefix = 'http://'
     URL = '127.0.0.1:5000' + url_for('posterpage')
-    return render_template('posterpage.html', data = session, imgURL = qrURL(prefix,URL),\
+    return render_template('posterpage.html', data = session, imgURL = qrURL(URL,prefix),\
     URL=URL, prefix=prefix, pngpath=pdf2png.pdfpreview(session['poster.posterfile'],\
     './static/img/'+session['poster.posterfile'].split('/')[-1]+'.png',subproc=True))
 
